f3062c6bf6d3c2757b0c3d8cd5ec5486dcc509e3
Mai Huu Hanh <hanhmh1203@gmail.com>
2018-08-26 10:21:41 +0700

    first commit
    add source

---
+++ app/src/main/java/readtoolead/egn/com/firebase_work1/MainActivity.kt
@@ -0,0 +1,12 @@
+package readtoolead.egn.com.firebase_work1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
